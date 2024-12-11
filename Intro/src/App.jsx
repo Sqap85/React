3b5d09533028e454ce4e065bf19d73d3620ef80d
Sqap85 <148553397+Sqap85@users.noreply.github.com>
2024-12-11 10:48:44 +0300

    85

---
+++ Intro/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+//javascript kodlari burada
+let x = 85;
+
+  return(
+    //html kodlari burada
+    <h1>Selam {x}</h1>
+  )
+}
+
+export default App
